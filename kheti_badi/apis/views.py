b32a0fe255f567db391b3f68f80d6d09a968903a
adityaagg7 <79642133+adityaagg7@users.noreply.github.com>
2022-11-25 10:23:33 +0530

    Update views.py

--- kheti_badi/apis/views.py
+++ kheti_badi/apis/views.py
@@ -1,72 +1,60 @@
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# import model
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import model
 import requests
 import json
 
-# # import Request 
-
-# # @api_view(['POST'])
-# def data_from_esp(request):
-#     global Data 
-#     Data=request
-#     soil_type = request.get('soil_type')
-#     crop_type = request.get('crop_type')
-#     temp = request.get('temp')
-#     humidity = request.get('humidity')
-#     moisture = request.get('moisture')
-#     potassium = request.get('potassium')
-#     nitrogen = request.get('nitrogen')
-#     phosphorous = request.get('phosphorous')
-#     lister=[temp,humidity,moisture,soil_type,crop_type,nitrogen,potassium,phosphorous]
-#     print(lister)
-#     pred=model.predict(lister)
-#     print("The Recommended URL is ",pred)
-#     return pred
-
-
-# @api_view(['GET'])
-# def send_data_to_front(request):
-#     list={"potassium":80, "nitrogen":90,"phosphorous":90}
-#     return Response(list)
-
+# import Request 
 
 # @api_view(['POST'])
-# def getFertilizer(request):
-#     farm_id=request.POST.get('farm_id')
-#     print(farm_id)
-#     dict={'soil_type':'sandy','temp':10,'crop_type':'maize','nitrogen':10,'potassium':10,'phosphorous':10,'humidity':10,'moisture':10}
-#     fert=data_from_esp(dict)
-#     return Response({'fert':fert})
-# @api_view(['GET'])
+def data_from_esp(request):
+    global Data 
+    Data=request
+    soil_type = request.get('soil_type')
+    crop_type = request.get('crop_type')
+    temp = request.get('temp')
+    humidity = request.get('humidity')
+    moisture = request.get('moisture')
+    potassium = request.get('potassium')
+    nitrogen = request.get('nitrogen')
+    phosphorous = request.get('phosphorous')
+    lister=[temp,humidity,moisture,soil_type,crop_type,nitrogen,potassium,phosphorous]
+    print(lister)
+    pred=model.predict(lister)
+    print("The Recommended URL is ",pred)
+    return pred
 
-# def rainfallStatus(request):
-#     api_key = "feb9b2de86f6d578ba83164f3854fdd6"
 
-#     base_url = "http://api.openweathermap.org/data/2.5/weather?"
+@api_view(['GET'])
+def send_data_to_front(request):
+    list={"potassium":80, "nitrogen":90,"phosphorous":90}
+    return Response(list)
+
+
+@api_view(['POST'])
+def getFertilizer(request):
+    farm_id=request.POST.get('farm_id')
+    print(farm_id)
+    dict={'soil_type':'sandy','temp':10,'crop_type':'maize','nitrogen':10,'potassium':10,'phosphorous':10,'humidity':10,'moisture':10}
+    fert=data_from_esp(dict)
+    return Response({'fert':fert})
+@api_view(['GET'])
+
+def rainfallStatus(request):
+    api_key = "feb9b2de86f6d578ba83164f3854fdd6"
+
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
     
-#     city_name = "Patiala"
+    city_name = "Patiala"
 
-#     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
     
-#     response = requests.get(complete_url)
+    response = requests.get(complete_url)
     
 
-#     x = response.json()
-#     print(x)
+    x = response.json()
+    x=x['weather']
+    return Response(x['main'])
+    
 
 
-Api_key = "d0999d6a4900e7462c6fb77beee9f1e5"
-
-
-base_url = "http://api.openweathermap.org/data/2.5/forecast?"
-
-city_name = "Patiala"
-
-complete_url = base_url + "appid=" + Api_key + "&q=" + city_name
-
-response = requests.get(complete_url)
-
-
-x = response.json()
-print(x['weather'])
